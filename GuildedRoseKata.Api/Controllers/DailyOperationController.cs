4d153d57129415937d6ec2806f1bccadcfbbf83e
Neil Michalares <neil.michalares@gmail.com>
2017-03-11 09:56:41 -0500

    remvoed values controller and added "daily Operations" boiler plate

---
+++ GuildedRoseKata.Api/Controllers/DailyOperationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuildedRoseKata.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class DailyOperationController : Controller
+    {
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+        
+    }
+}
